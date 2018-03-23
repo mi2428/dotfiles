34f12635924336b0c536473ab4e3fad62ef7caf1
mi2428 <mi2428782020@gmail.com>
2018-03-23 23:30:37 +0900

    minor update
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- etc/hosts/yoga/i3/i3/i3blocks/audioctl.sh
+++ etc/hosts/yoga/i3/i3/i3blocks/audioctl.sh
@@ -12,11 +12,11 @@ case $1 in
             pacmd set-sink-mute $device 0 || pacmd set-sink-mute $device 1
         ;;
     "up")
-        setvol=$(($current + 5000))
+        setvol=$(($current + 3250))
         (( $setvol >= 65000 )) && setvol=65000
         pacmd set-sink-volume $device $setvol ;;
     "down")
-        setvol=$(($current - 5000))
+        setvol=$(($current - 3250))
         (( $setvol <= 0 )) && setvol=0
         pacmd set-sink-volume $device $setvol ;;
 esac
