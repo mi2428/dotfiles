e01cb1f3d5b3e757e3a43ed4d96527407d14ce35
mi2428 <mi2428782020@gmail.com>
2018-05-29 15:12:20 +0900

    Support multiple interfaces and multiple ipv4 addresses
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- etc/hosts/yoga/i3/i3/i3blocks/eth.sh
+++ etc/hosts/yoga/i3/i3/i3blocks/eth.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-INTERFACE=$(ip link show | grep -m 1 -o "enp0s31f6")
-ADDR=$(ip addr show dev $INTERFACE | grep "global" | awk '{print $2}')
-if [[ "$ADDR" == "" ]]; then
-    # echo -e "Not-Connected\n\n#FE2E2E"
+INTERFACES=$(ip link show | grep "state UP" | awk '{print $2}' | grep -E "^e.*$" | tr -d ':')
+if [[ -z $INTERFACES ]]; then
     echo -e "N/A\n\n#FE2E2E"
 else
-    if [[ $(ip route show | sed -n '1,1p' | grep $INTERFACE) != "" ]]; then
-        echo -e "${ADDR} (route)\n\n#8CFF2D"
-    else
-        echo -e "${ADDR}\n\n#8CFF2D"
-    fi
+    IP=""
+    for interface in ${INTERFACES[@]}; do
+        ipaddr=$(ip addr show dev $interface | grep -E -o '([[:digit:]]{1,3}.){3}[[:digit:]]{1,3}/[[:digit:]]{1,2}' | tr '\n' ' ')
+        IP="$ipaddr $IP"
+    done
+    echo -e "${IP}\n\n#8CFF2D"
 fi
