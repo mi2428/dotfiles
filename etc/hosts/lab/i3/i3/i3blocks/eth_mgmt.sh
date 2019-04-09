94ebd39d4cad09f246982631bc4c41bd624626c2
mi2428 <mi2428782020@gmail.com>
2019-04-09 16:59:54 +0900

    update
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

---
+++ etc/hosts/lab/i3/i3/i3blocks/eth_mgmt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ADDR=$(ip addr show | grep "internal@eno1" -A 5 | grep "scope global internal" | awk '{print $2}')
+if [[ "$ADDR" == "" ]]; then
+    # echo -e "Not-Connected\n\n#FE2E2E"
+    echo -e "N/A\n\n#FE2E2E"
+else
+    if [[ $(ip route show | sed -n '1,1p' | grep $INTERFACE) != "" ]]; then
+        echo -e "${ADDR} (route)\n\n#8CFF2D"
+    else
+        echo -e "${ADDR}\n\n#8CFF2D"
+    fi
+fi
