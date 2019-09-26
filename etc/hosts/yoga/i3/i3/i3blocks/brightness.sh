ec74f80abec04f7766babd6e8d64633e3689c116
mi2428 <mi2428782020@gmail.com>
2019-09-26 12:24:41 +0900

    Update
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- etc/hosts/yoga/i3/i3/i3blocks/brightness.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-brightness=$(echo "scale=1; `cat /sys/class/backlight/intel_backlight/brightness`/10" | bc -l)
-echo $brightness%
--- etc/hosts/yoga/i3/i3/i3blocks/load.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-load=$(cut -d ' ' -f3 /proc/loadavg)
-echo $load
