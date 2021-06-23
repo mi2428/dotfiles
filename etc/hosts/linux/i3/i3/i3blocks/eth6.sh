19baacd3b2e741a59b715320c2d7689441001593
mi2428 <mi2428782020@gmail.com>
2021-06-24 01:04:44 +0900

    tmp
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- etc/hosts/linux/i3/i3/i3blocks/eth6.sh
+++ etc/hosts/linux/i3/i3/i3blocks/eth6.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ADDR=$(ip addr show | grep "br0:" -A 5 | grep "inet6" | grep "scope global" | awk '{print $2}')
+ADDR=$(ip addr show | grep "eth0:" -A 5 | grep "inet6" | grep "scope global" | awk '{print $2}')
 if [[ "$ADDR" == "" ]]; then
     echo -e "N/A\n\n#FE2E2E"
 else
