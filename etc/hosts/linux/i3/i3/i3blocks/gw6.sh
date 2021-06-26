7f16fac28bc11a5e6ad463788be010af76e67bbd
mi2428 <mi2428782020@gmail.com>
2021-06-27 00:27:09 +0900

    Fix
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- etc/hosts/linux/i3/i3/i3blocks/gw6.sh
+++ etc/hosts/linux/i3/i3/i3blocks/gw6.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-gw=$(ip -6 route show | grep "default" | sed -n 1p | sed -e 's/^.*via \(\S*\).*$/\1/g')
+
+#gw=$(ip -6 route show default | sed -n 1p | sed -e 's/^.*via \(\S*\).*$/\1/g')
+gw=$(ip -6 r show default | grep via | cut -d " " -f 3 | sed -n 1p)
 [[ $gw != "" ]] && echo -e "$gw\n\n#8CFF2D" || echo -e "N/A\n\n#FE2E2E"
