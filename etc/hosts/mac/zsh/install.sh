765e9b68c43b6280bad0ae028d4043ae534ee0db
mi2428 <mi2428782020@gmail.com>
2022-02-04 02:17:28 +0900

    keep

---
+++ etc/hosts/mac/zsh/install.sh
@@ -0,0 +1,10 @@
+brew install zsh-cimpletions
+chmod -R go-w /opt/homebrew/share
+brew install zsh-autosuggestions
+brew install zsh-git-prompt
+brew install exa bat procs hexyl ripgrep tokei
+cargo install fd-find
+brew install zsh-syntax-highlighting
+brew install grc
+brew install most
+brew install homebrew/cask-fonts/font-hack-nerd-font
