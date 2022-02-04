b07a3b5bc997bc03119c30cbe6852bb0279f0454
mi2428 <mi2428782020@gmail.com>
2022-02-05 01:01:50 +0900

    install neovim

--- etc/hosts/mac/zsh/install.sh
+++ etc/hosts/mac/zsh/install.sh
@@ -9,3 +9,4 @@ brew install grc
 brew install most
 brew install homebrew/cask-fonts/font-hack-nerd-font
 git clone https://github.com/tmux-plugins/tpm /Users/mi/.tmux/plugins/tpm
+sudo pip3 install neovim pynvim
