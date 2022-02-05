338259e3c6beced108e91e7cde87de7c0f78bbb2
mi2428 <mi2428782020@gmail.com>
2022-02-05 16:31:19 +0900

    update install.sh

---
+++ etc/hosts/mac/install.sh
@@ -0,0 +1,14 @@
+brew install zsh-cimpletions
+chmod -R go-w /opt/homebrew/share
+brew install zsh-autosuggestions
+brew install zsh-git-prompt
+brew install exa bat procs hexyl ripgrep tokei
+cargo install fd-find
+brew install zsh-syntax-highlighting
+brew install grc
+brew install most
+brew install homebrew/cask-fonts/font-hack-nerd-font
+git clone https://github.com/tmux-plugins/tpm /Users/mi/.tmux/plugins/tpm
+sudo pip3 install neovim pynvim
+brew install trash
+brew install ipinfo
