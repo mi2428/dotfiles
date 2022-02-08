34d78f41a05450938f96cb9a0e1a97d8687b4286
mi2428 <mi2428782020@gmail.com>
2022-02-08 16:23:41 +0900

    add fzf completion

--- etc/hosts/mac/zsh/zsh/10_general.zsh
+++ etc/hosts/mac/zsh/zsh/10_general.zsh
@@ -33,13 +33,92 @@ export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export LC_TERMINAL=iTerm2
 
+
+case $(arch) in
+x86_64)
+  export VOLTA_HOME="$HOME/.volta_x64"
+  typeset -U path PATH
+  path=(
+    $HOME/bin
+    $VOLTA_HOME/bin
+    /usr/local/bin(N-/)   # use /usr/local/bin in preference to /usr/bin in Intel Mac
+    /usr/local/sbin(N-/)  # use /usr/local/sbin in preference to /usr/sbin in Intel Mac
+    /usr/bin
+    /usr/sbin
+    /bin
+    /sbin
+    /Library/Apple/usr/bin
+  )
+  ;;
+arm64*)
+  export CARGO_HOME="$HOME/.cargo"
+  export VOLTA_HOME="$HOME/.volta"
+  export TEXLIVE_BIN="/usr/local/texlive/2021/bin/universal-darwin"
+  export FZF_BIN="/opt/homebrew/opt/fzf/bin"
+  typeset -U path PATH
+  path=(
+    $HOME/bin
+    $CARGO_HOME/bin
+    $VOLTA_HOME/bin
+    $TEXLIVE_BIN
+    $FZF_BIN
+    /opt/homebrew/bin(N-/)
+    /opt/homebrew/sbin(N-/)
+    /usr/bin
+    /usr/sbin
+    /bin
+    /sbin
+    /usr/local/bin
+    /usr/local/sbin
+    /Library/Apple/usr/bin
+  )
+  ;;
+esac
+
+
+export GOPATH="$HOME/dev/gocode"
 export FPATH=/opt/homebrew/share/zsh-completions:$FPATH
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=242'
 export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/opt/homebrew/share/zsh-syntax-highlighting/highlighters
 source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /opt/homebrew/opt/zsh-git-prompt/zshrc.sh
 source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source $HOME/.fzf.zsh
+
+
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+
+export FZF_COMPLETION_TRIGGER='**'
+export FZF_COMPLETION_OPTS='--border --info=inline'
+
+# Use fd (https://github.com/sharkdp/fd) instead of the default find
+# command for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+  fd --hidden --follow --exclude ".git" . "$1"
+}
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+}
+
+# (EXPERIMENTAL) Advanced customization of fzf options via _fzf_comprun function
+# - The first argument to the function is the name of the command.
+# - You should make sure to pass the rest of the arguments to fzf.
+_fzf_comprun() {
+  local command=$1
+  shift
+
+  case "$command" in
+    cd)           fzf "$@" --preview 'tree -C {} | head -200' ;;
+    export|unset) fzf "$@" --preview "eval 'echo \$'{}" ;;
+    ssh)          fzf "$@" --preview 'dig {}' ;;
+    *)            fzf "$@" ;;
+  esac
+}
+
 
 autoload -Uz colors && colors
 autoload -Uz compinit && compinit
@@ -77,7 +156,7 @@ fi
 
 
 export EDITOR="vi"
-bindkey -e  # set explicitly, or zsh use vi-mode binding
+bindkey -e  # set explicitly, or zsh use vi-mode binding by default
 bindkey '^O' autosuggest-accept
 
 
@@ -86,50 +165,7 @@ backward-kill-dir() {
   zle backward-kill-word
 }
 zle -N backward-kill-dir
-bindkey '^D' backward-kill-dir
-
-
-export GOPATH="$HOME/dev/gocode"
-
-
-case $(arch) in
-x86_64)
-  export VOLTA_HOME="$HOME/.volta_x64"
-  typeset -U path PATH
-  path=(
-    $HOME/bin
-    $VOLTA_HOME/bin
-    /usr/local/bin(N-/)   # use /usr/local/bin in preference to /usr/bin in Intel Mac
-    /usr/local/sbin(N-/)  # use /usr/local/sbin in preference to /usr/sbin in Intel Mac
-    /usr/bin
-    /usr/sbin
-    /bin
-    /sbin
-    /Library/Apple/usr/bin
-  )
-  ;;
-arm64*)
-  export CARGO_HOME="$HOME/.cargo"
-  export VOLTA_HOME="$HOME/.volta"
-  export TEXLIVE_BIN="/usr/local/texlive/2021/bin/universal-darwin"
-  typeset -U path PATH
-  path=(
-    $HOME/bin
-    $CARGO_HOME/bin
-    $VOLTA_HOME/bin
-    $TEXLIVE_BIN
-    /opt/homebrew/bin(N-/)
-    /opt/homebrew/sbin(N-/)
-    /usr/bin
-    /usr/sbin
-    /bin
-    /sbin
-    /usr/local/bin
-    /usr/local/sbin
-    /Library/Apple/usr/bin
-  )
-  ;;
-esac
+bindkey '^W' backward-kill-dir  # remap
 
 
 if [[ -n ${SSH_AGENT_PID} ]] && ! ssh-add -l 1> /dev/null; then
