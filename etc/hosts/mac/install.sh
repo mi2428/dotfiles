9940b7a8b3acea37413f8b3eef4064618a47be9e
mi2428 <mi2428782020@gmail.com>
2022-02-08 16:34:39 +0900

    fix

--- etc/hosts/mac/install.sh
+++ etc/hosts/mac/install.sh
@@ -8,7 +8,6 @@ brew install zsh-syntax-highlighting
 brew install grc
 brew install most
 brew install homebrew/cask-fonts/font-hack-nerd-font
-git clone https://github.com/tmux-plugins/tpm /Users/mi/.tmux/plugins/tpm
 sudo pip3 install neovim pynvim
 brew install trash
 brew install ipinfo
