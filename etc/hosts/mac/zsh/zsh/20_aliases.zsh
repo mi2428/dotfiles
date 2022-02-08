88b0c0ed35c826cfba1466d56103b503259d3e9e
mi2428 <mi2428782020@gmail.com>
2022-02-08 17:55:12 +0900

    update fzf

--- etc/hosts/mac/zsh/zsh/20_aliases.zsh
+++ etc/hosts/mac/zsh/zsh/20_aliases.zsh
@@ -84,13 +84,29 @@ xx() {
 }
 
 
-function gadd() {
+gadd() {
   local selected
   selected=$(unbuffer git status -s | fzf -m --ansi --preview="echo {} | awk '{print \$2}' | xargs git diff --color" | awk '{print $2}')
   if [[ -n "${selected}" ]]; then
     selected=$(tr '\n' ' ' <<< "${selected}")
     git add ${selected}
     echo "Completed: git add ${selected}"
+  fi
+}
+
+
+fe() {
+  local files
+  IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
+  [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
+}
+
+
+fkill() {
+  local pid
+  pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+  if [ "x$pid" != "x" ]; then
+    echo $pid | xargs kill -${1:-9}
   fi
 }
 
