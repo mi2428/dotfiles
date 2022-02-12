46aaf632697626e955da650eaf4564a55d45bbb9
mi2428 <mi2428782020@gmail.com>
2022-02-12 16:12:44 +0900

    add remote kick

--- init/kick.sh
+++ init/kick.sh
@@ -1,3 +1,23 @@
-git clone https://github.com/mi2428/dotfiles -O $HOME/dotfiles
+#!/bin/bash
+cd $HOME
+
+DIST=""
+
+if [[ -f /etc/lsb-release ]]; then
+  DIST=$(grep DISTRIBUTION_ID <<< /etc/lsb-release | cut -d '=' -f 2)
+fi
+
+case $DIST; in
+  Ubuntu)
+    apt install git make sudo ;;
+  *)
+    exit 127 ;;
+esac
+
+git clone --depth 1 https://github.com/mi2428/dotfiles -O $HOME/dotfiles
 cd $HOME/dotfiles
-make install link
+
+case $DIST; in
+  Ubuntu)
+    make ubuntu-server ;;
+esac
