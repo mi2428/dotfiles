a3d9b9461f1e61b0f16f3264ed8bd1384dba3d8c
mi2428 <mi2428782020@gmail.com>
2022-02-13 01:00:04 +0900

    fix name resolution issue

--- var/entrypoint.sh
+++ var/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 HOST_USER=${HOST_USER:-shizk}
 
+echo "127.0.1.1 ${HOSTNAME}" >> /etc/hosts
 useradd -m -k /etc/skel -u $HOST_UID -g $HOST_GID -G wheel -s /bin/zsh ${HOST_USER}
 cd /work
 
