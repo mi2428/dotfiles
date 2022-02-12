d46c985fb0dbfde29c92f434b2457e97216091cd
mi2428 <mi2428782020@gmail.com>
2022-02-13 00:45:49 +0900

    fix option

--- var/entrypoint.sh
+++ var/entrypoint.sh
@@ -4,4 +4,4 @@ HOST_USER=${HOST_USER:-shizk}
 useradd -m -k /etc/skel -u $HOST_UID -g $HOST_GID -G wheel -s /bin/zsh ${HOST_USER}
 cd /work
 
-exec sudo -E -u ${HOST_USER} $@
+exec sudo -u ${HOST_USER} $@
