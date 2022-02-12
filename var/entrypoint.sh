460bf31e72c5c8bbdbb3535d64bcb01ef37ae00a
mi2428 <mi2428782020@gmail.com>
2022-02-13 02:06:58 +0900

    create group if not exists

--- var/entrypoint.sh
+++ var/entrypoint.sh
@@ -2,6 +2,7 @@
 HOST_USER=${HOST_USER:-shizk}
 
 echo "127.0.1.1 ${HOSTNAME}" >> /etc/hosts
+groupadd -g $HOST_GID ${HOST_USER} || true
 useradd -m -k /etc/skel -u $HOST_UID -g $HOST_GID -G wheel -s /bin/zsh ${HOST_USER}
 cd /work
 
