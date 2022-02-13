5e3712e1997fea67461e3e8a3345610730dfa4ec
mi2428 <mi2428782020@gmail.com>
2022-02-13 13:47:17 +0900

    suppress warning

--- var/entrypoint.sh
+++ var/entrypoint.sh
@@ -2,7 +2,7 @@
 HOST_USER=${HOST_USER:-shizk}
 
 echo "127.0.1.1 ${HOSTNAME}" >> /etc/hosts
-groupadd -g $HOST_GID ${HOST_USER} || true
+groupadd -g $HOST_GID ${HOST_USER} 2>/dev/null || true
 useradd -m -k /etc/skel -u $HOST_UID -g $HOST_GID -G wheel -s /bin/zsh ${HOST_USER}
 cd /work
 
