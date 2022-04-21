c7842d20e179864b3a67f4696b344ca6186fc64c
mi2428 <mi2428782020@gmail.com>
2022-04-22 00:43:51 +0900

    Remove hard coding

--- etc/hosts/macos/tmux/tmux/scripts/ssd.sh
+++ etc/hosts/macos/tmux/tmux/scripts/ssd.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
-total=$(diskutil info /dev/disk3s1s1 | grep "Container Total Space:" | sed 's/^.*(exactly \([0-9]*\) 512-Byte-Units)$/\1/')
-avail=$(diskutil info /dev/disk3s1s1 | grep "Container Free Space:" | sed 's/^.*(exactly \([0-9]*\) 512-Byte-Units)$/\1/')
+diskname=$(df / | sed -n 2,2p | cut -d ' ' -f 1)
+total=$(diskutil info $diskname | grep "Container Total Space:" | sed 's/^.*(exactly \([0-9]*\) 512-Byte-Units)$/\1/')
+avail=$(diskutil info $diskname | grep "Container Free Space:" | sed 's/^.*(exactly \([0-9]*\) 512-Byte-Units)$/\1/')
 printf '%.0f%%\n' $(( 100 * (total - avail) / total ))
