aa35a6f472e4f720a70f4bff5721169308397a34
mi2428 <mi2428782020@gmail.com>
2022-08-25 18:00:32 +0900

    Fix

--- etc/hosts/macos/tmux/tmux/scripts/ipv4.sh
+++ etc/hosts/macos/tmux/tmux/scripts/ipv4.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-src=$(ip r get 1.1.1.1 | \grep -Eo "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sed -n "3,3p" 2> /dev/null)
+src=$(ip r get 1.1.1.1 | \grep -Eo "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | tail -n 1 2> /dev/null)
 if [[ -n $src ]]; then
   echo "$src"
 else
