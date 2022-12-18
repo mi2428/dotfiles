9e1c4bda6bf96f7abfc7c14213f6223e2a55e004
mi2428 <mi2428782020@gmail.com>
2022-12-18 20:58:09 +0900

    Add fzf path
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- etc/zsh/zsh/12_general_path.zsh
+++ etc/zsh/zsh/12_general_path.zsh
@@ -1,6 +1,7 @@
 export DENO_INSTALL=$HOME/.deno
 export CARGO_HOME="$HOME/.cargo"
 export VOLTA_HOME="$HOME/.volta"
+export FZF_HOME="$HOME/.fzf"
 typeset -U path PATH
 path=(
   $HOME/bin
@@ -9,6 +10,7 @@ path=(
   $DENO_INSTALL/bin
   $CARGO_HOME/bin
   $VOLTA_HOME/bin
+  $FZF_HOME/bin
   /usr/bin
   /usr/sbin
   /bin
