fb92afc9e19d219b8a5eceb024057b27d6ace23b
mi2428 <mi2428782020@gmail.com>
2025-03-14 16:20:52 +0900

    mod: add tmuxinator completion
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- etc/zsh/zsh/10_general.zsh
+++ etc/zsh/zsh/10_general.zsh
@@ -179,3 +179,27 @@ autoload -Uz compinit && compinit  # all completion settings must be done before
 
 # https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#enable-shell-autocompletion
 source <(kubectl completion zsh)
+
+# https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.zsh
+_tmuxinator() {
+  local commands projects
+  commands=(${(f)"$(tmuxinator commands zsh)"})
+  projects=(${(f)"$(tmuxinator completions start)"})
+
+  if (( CURRENT == 2 )); then
+    _alternative \
+      'commands:: _describe -t commands "tmuxinator subcommands" commands' \
+      'projects:: _describe -t projects "tmuxinator projects" projects'
+  elif (( CURRENT == 3)); then
+    case $words[2] in
+      copy|cp|c|debug|delete|rm|open|o|start|s|edit|e)
+        _arguments '*:projects:($projects)'
+      ;;
+    esac
+  fi
+
+  return
+}
+
+compdef _tmuxinator tmuxinator
+
