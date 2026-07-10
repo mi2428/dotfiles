c36e30de9e5d51403c4eedf21ffbc05074b95c39
mi2428 <mi2428782020@gmail.com>
2026-07-10 12:29:16 +0900

    feat: add herdr config and fish grc wrappers

---
+++ etc/fish/conf.d/40_grc.fish
@@ -0,0 +1,102 @@
+if not command -sq grc
+    return
+end
+
+if not status is-interactive
+    return
+end
+
+if test -z "$TERM"; or test "$TERM" = dumb
+    return
+end
+
+set -l grc_cmds \
+    as \
+    ant \
+    blkid \
+    cc \
+    configure \
+    curl \
+    cvs \
+    df \
+    diff \
+    dig \
+    dnf \
+    docker \
+    docker-compose \
+    docker-machine \
+    du \
+    fdisk \
+    findmnt \
+    free \
+    g++ \
+    gas \
+    gcc \
+    getfacl \
+    getsebool \
+    gmake \
+    id \
+    ifconfig \
+    iostat \
+    ip \
+    iptables \
+    iwconfig \
+    journalctl \
+    kubectl \
+    last \
+    ldap \
+    lolcat \
+    ld \
+    lsattr \
+    lsblk \
+    lsmod \
+    lsof \
+    lspci \
+    make \
+    mount \
+    mtr \
+    mvn \
+    netstat \
+    nmap \
+    ntpdate \
+    php \
+    ping \
+    ping6 \
+    proftpd \
+    ps \
+    sar \
+    semanage \
+    sensors \
+    showmount \
+    sockstat \
+    ss \
+    stat \
+    sysctl \
+    systemctl \
+    traceroute \
+    traceroute6 \
+    tune2fs \
+    ulimit \
+    uptime \
+    vmstat \
+    wdiff \
+    whois
+
+for cmd in $grc_cmds
+    if not command -sq $cmd
+        continue
+    end
+
+    # Keep explicit aliases/functions such as ls=exa intact.
+    if functions -q $cmd
+        continue
+    end
+
+    set -l escaped (string escape -- $cmd)
+    eval "
+        function $escaped --wraps '$escaped'
+            grc --colour=auto $escaped \$argv
+        end
+    "
+end
+
