93c19c5804e374184e1ae9db664f942d3eaff679
mi2428 <mi2428782020@gmail.com>
2026-07-12 17:26:57 +0900

    feat: streamline bufferline window mappings

--- etc/nvim/lua/config/keymaps.lua
+++ etc/nvim/lua/config/keymaps.lua
@@ -1,8 +1,30 @@
 local map = vim.keymap.set
 
-map("n", ";;", "<cmd>tabnew<cr>", { desc = "New tab" })
-map("n", "<S-l>", "<cmd>tabnext<cr>", { desc = "Next tab" })
-map("n", "<S-h>", "<cmd>tabprevious<cr>", { desc = "Previous tab" })
+local function bufferline_group_action()
+	local groups = {}
+	for _, group in ipairs(require("bufferline.groups").get_names(true)) do
+		if group ~= "pinned" and group ~= "ungrouped" then
+			groups[#groups + 1] = group
+		end
+	end
+	if #groups == 0 then
+		return
+	end
+
+	vim.ui.select({ "toggle", "close" }, { prompt = "Bufferline group action" }, function(action)
+		if not action then
+			return
+		end
+		local command = action == "toggle" and "BufferLineGroupToggle" or "BufferLineGroupClose"
+
+		vim.ui.select(groups, { prompt = "Bufferline group" }, function(group)
+			if group then
+				vim.cmd(command .. " " .. group)
+			end
+		end)
+	end)
+end
+
 map("n", "<C-l>", "<cmd>nohlsearch<cr>", { desc = "Clear search highlight" })
 map("x", "p", [["_dP]], { desc = "Paste without yanking replaced text" })
 
@@ -11,9 +33,12 @@ map("n", "-", function()
 end, { desc = "Open parent directory" })
 map("n", "<C-w>h", "<cmd>BufferLineCyclePrev<cr>", { desc = "Previous buffer" })
 map("n", "<C-w>l", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
-map("n", "<C-w>j", "<cmd>tabprevious<cr>", { desc = "Previous tab" })
-map("n", "<C-w>k", "<cmd>tabnext<cr>", { desc = "Next tab" })
 map("n", "<C-w>p", "<cmd>BufferLineTogglePin<cr>", { desc = "Toggle buffer pin" })
+map("n", "<C-w>f", "<cmd>BufferLinePick<cr>", { desc = "Pick buffer" })
+map("n", "<C-w>d", "<cmd>BufferLinePickClose<cr>", { desc = "Pick buffer to close" })
+map("n", "<C-w>,", "<cmd>BufferLineMovePrev<cr>", { desc = "Move buffer left" })
+map("n", "<C-w>.", "<cmd>BufferLineMoveNext<cr>", { desc = "Move buffer right" })
+map("n", "<C-w>g", bufferline_group_action, { desc = "Bufferline group action" })
 map("n", "<C-w>-", "<cmd>split<cr>", { desc = "Horizontal split" })
 map("n", "<C-w>|", "<cmd>vsplit<cr>", { desc = "Vertical split" })
 for i = 1, 9 do
