f1193fad517ddf83c799c1300dae318aebcca5cd
mi2428 <mi2428782020@gmail.com>
2026-07-12 23:16:30 +0900

    feat: add aws region toggles to dial.nvim

--- etc/nvim/lua/plugins/core.lua
+++ etc/nvim/lua/plugins/core.lua
@@ -77,6 +77,42 @@ return {
 		event = "VeryLazy",
 		config = function()
 			local augend = require("dial.augend")
+			local aws_regions = {
+				"us-east-1",
+				"us-east-2",
+				"us-west-1",
+				"us-west-2",
+				"af-south-1",
+				"ap-east-1",
+				"ap-south-2",
+				"ap-southeast-3",
+				"ap-southeast-5",
+				"ap-southeast-4",
+				"ap-south-1",
+				"ap-southeast-6",
+				"ap-northeast-3",
+				"ap-northeast-2",
+				"ap-southeast-1",
+				"ap-southeast-2",
+				"ap-east-2",
+				"ap-southeast-7",
+				"ap-northeast-1",
+				"ca-central-1",
+				"ca-west-1",
+				"eu-central-1",
+				"eu-west-1",
+				"eu-west-2",
+				"eu-south-1",
+				"eu-west-3",
+				"eu-south-2",
+				"eu-north-1",
+				"eu-central-2",
+				"il-central-1",
+				"mx-central-1",
+				"me-south-1",
+				"me-central-1",
+				"sa-east-1",
+			}
 
 			require("dial.config").augends:register_group({
 				default = {
@@ -95,6 +131,11 @@ return {
 						word = true,
 						cyclic = true,
 						preserve_case = true,
+					}),
+					augend.constant.new({
+						elements = aws_regions,
+						pattern_regexp = [[\C\%%(^\|[^-0-9a-z]\)\zs\(%s\)\ze\%%($\|[^-0-9a-z]\)]],
+						cyclic = true,
 					}),
 				},
 			})
