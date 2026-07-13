59a35d44c7f92bedcebc02e065d9d81522ed5b2c
mi2428 <mi2428782020@gmail.com>
2026-07-13 23:34:49 +0900

    refactor: home/files/config/fish/conf.d/12_general_path.macos.fish

--- home/files/config/fish/conf.d/12_general_path.macos.fish
+++ home/files/config/fish/conf.d/12_general_path.macos.fish
@@ -1,14 +1,14 @@
-set -gx DENO_INSTALL $HOME/.deno
-set -gx CARGO_HOME $HOME/.cargo
-set -gx GOPATH $HOME/io/gocode
+set -gx DENO_INSTALL "$HOME/.deno"
+set -gx CARGO_HOME "$HOME/.cargo"
+set -gx GOPATH "$HOME/io/gocode"
 set -gx GOPRIVATE github.com/soracom
 
-__dotfiles_import_posix_exports $HOME/.envs
+__dotfiles_import_posix_exports "$HOME/.envs"
 
 switch (uname -m)
     case x86_64 i386
         set -gx HOMEBREW_HOME /usr/local
-        set -gx VOLTA_HOME $HOME/.volta_x64
+        set -gx VOLTA_HOME "$HOME/.volta_x64"
 
         __dotfiles_set_path \
             $HOME/bin \
@@ -34,7 +34,7 @@ switch (uname -m)
             ./bin
     case arm64 '*'
         set -gx HOMEBREW_HOME /opt/homebrew
-        set -gx VOLTA_HOME $HOME/.volta
+        set -gx VOLTA_HOME "$HOME/.volta"
         set -gx TEXLIVE_BIN /usr/local/texlive/2022/bin/universal-darwin
 
         __dotfiles_set_path \
@@ -64,7 +64,7 @@ switch (uname -m)
             ./bin
 end
 
-set -gx SDKMAN_DIR $HOME/.sdkman
+set -gx SDKMAN_DIR "$HOME/.sdkman"
 
 if command -sq rbenv
     rbenv init - --no-rehash fish | source
