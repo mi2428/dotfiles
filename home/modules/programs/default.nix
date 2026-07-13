8d4c07feb2abed67d1882ce62bef93c93fb5ae64
mi2428 <mi2428782020@gmail.com>
2026-07-13 23:06:15 +0900

    fix: keep latexmkrc unmanaged

--- home/modules/programs/default.nix
+++ home/modules/programs/default.nix
@@ -3,7 +3,6 @@
     ./direnv.nix
     ./fish.nix
     ./git.nix
-    ./latexmk.nix
     ./nvim.nix
     ./tmux.nix
     ./zsh.nix
--- home/modules/programs/latexmk.nix
+++
@@ -1,7 +0,0 @@
-{ lib, pkgs, ... }:
-lib.mkIf pkgs.stdenv.isDarwin {
-  home.file.".latexmkrc" = {
-    force = true;
-    source = ../../files/latexmk/latexmkrc;
-  };
-}
