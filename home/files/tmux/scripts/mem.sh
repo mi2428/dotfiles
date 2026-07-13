b2c2bae0fd408880ab8fcb227165206840bdfa9f
mi2428 <mi2428782020@gmail.com>
2026-07-13 23:30:48 +0900

    refactor: home/files/tmux/scripts/mem.sh

--- home/files/tmux/scripts/mem.sh
+++ home/files/tmux/scripts/mem.sh
@@ -1,3 +1,10 @@
 #!/bin/zsh
-used=$(free | grep "Mem:" | awk '{print 100*$3/$2}')
-printf '%.0f%%\n' $used
+
+used_percent="$(free | awk '/^Mem:/ {print 100 * $3 / $2; exit}')"
+
+if [[ -z "$used_percent" ]]; then
+  printf '0%%\n'
+  exit 0
+fi
+
+printf '%.0f%%\n' "$used_percent"
