3cec2e31f91520104ea8f7594321e3d153164d6c
mi2428 <mi2428782020@gmail.com>
2026-07-13 23:34:49 +0900

    refactor: home/files/config/fish/conf.d/15_fzf_theme.fish

--- home/files/config/fish/conf.d/15_fzf_theme.fish
+++ home/files/config/fish/conf.d/15_fzf_theme.fish
@@ -10,12 +10,12 @@ set -l fzf_theme "catppuccin-fzf-$DOTFILES_CATPPUCCIN_FLAVOUR.rc"
 set -l fzf_theme_file "$HOME/.config/fzf/themes/$fzf_theme"
 set -l source_file (path resolve (status filename))
 set -l etc_root (path dirname (path dirname (path dirname $source_file)))
-if not test -f $fzf_theme_file
+if not test -f "$fzf_theme_file"
     set fzf_theme_file "$etc_root/fzf/themes/$fzf_theme"
 end
 
-if test -f $fzf_theme_file
-    set -gx FZF_DEFAULT_OPTS_FILE $fzf_theme_file
+if test -f "$fzf_theme_file"
+    set -gx FZF_DEFAULT_OPTS_FILE "$fzf_theme_file"
 else
     set -e FZF_DEFAULT_OPTS_FILE
 end
