7d2d2f4924d540ba68ac0b4c10e3bc35dfae9e59
mi2428 <mi2428782020@gmail.com>
2026-07-13 18:24:15 +0900

    refactor: drop unmanaged zsh init hooks

--- home/modules/programs/zsh.nix
+++ home/modules/programs/zsh.nix
@@ -33,34 +33,11 @@ in {
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-    initContent = lib.mkMerge [
-      (lib.mkBefore ''
-        [[ -f "$HOME/.fig/shell/zshrc.pre.zsh" ]] && . "$HOME/.fig/shell/zshrc.pre.zsh"
-      '')
-      ''
-        for conf in "$HOME"/.zsh/*.zsh(N); do
-          source "$conf"
-        done
-
-        if [ -f "$HOME/.zshrc_functions/assume-role" ]; then
-          export SORACOM_AWS_LOGIN_ITEM_ID=jggxzohgplnedvzyftmxinlgdu
-          source "$HOME/.zshrc_functions/assume-role"
-        fi
-
-        export SDKMAN_DIR="$HOME/.sdkman"
-        [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
-
-        if [[ "$TERM_PROGRAM" == "kiro" ]] && command -v kiro >/dev/null 2>&1; then
-          kiro_shell_integration_path="$(kiro --locate-shell-integration-path zsh 2>/dev/null)"
-          [[ -n "$kiro_shell_integration_path" && -f "$kiro_shell_integration_path" ]] && . "$kiro_shell_integration_path"
-        fi
-
-        [[ -f "$HOME/.openclaw/completions/openclaw.zsh" ]] && source "$HOME/.openclaw/completions/openclaw.zsh"
-      ''
-      (lib.mkAfter ''
-        [[ -f "$HOME/.fig/shell/zshrc.post.zsh" ]] && . "$HOME/.fig/shell/zshrc.post.zsh"
-      '')
-    ];
+    initContent = ''
+      for conf in "$HOME"/.zsh/*.zsh(N); do
+        source "$conf"
+      done
+    '';
   };
 
   home.file = zshFiles;
