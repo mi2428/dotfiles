fba58fa930f709fa7a81b9f2809dc6d90a13140a
mi2428 <mi2428782020@gmail.com>
2026-07-13 18:18:41 +0900

    fix: force managed karabiner config

--- home/modules/programs/karabiner.nix
+++ home/modules/programs/karabiner.nix
@@ -1,5 +1,7 @@
 { hostName, lib, pkgs, ... }:
 lib.mkIf (pkgs.stdenv.isDarwin && hostName == "macos") {
-  xdg.configFile."karabiner/karabiner.json".source =
-    ../../../home/files/hosts/macos/karabiner/karabiner.json;
+  xdg.configFile."karabiner/karabiner.json" = {
+    force = true;
+    source = ../../../home/files/hosts/macos/karabiner/karabiner.json;
+  };
 }
