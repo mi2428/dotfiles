b1380a7dbaa55e80aa63030c4006492707c450e9
mi2428 <mi2428782020@gmail.com>
2026-07-13 23:30:48 +0900

    refactor: home/files/tmux/scripts/cpu.sh

--- home/files/tmux/scripts/cpu.sh
+++ home/files/tmux/scripts/cpu.sh
@@ -1,3 +1,11 @@
 #!/bin/zsh
-cpucore=$(lscpu | grep -o '^CPU(s):.*\([0-9]\)$' | awk '{print $NF}')
-printf '%.0f%%\n' $(( 100 * $(w | head -n 1 | awk '{print $NF}') / $cpucore ))
+
+cores="${TMUX_CPU_CORES:-$(nproc 2>/dev/null || lscpu | awk '/^CPU\\(s\\):/ {print $2; exit}')}"
+load_avg="$(uptime | awk '{print $NF}')"
+
+if [[ -z "$cores" || "$cores" == "0" || -z "$load_avg" ]]; then
+  printf '0%%\n'
+  exit 0
+fi
+
+printf '%.0f%%\n' $((100 * load_avg / cores))
