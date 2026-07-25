3eb161754e93a4a60ce0611817670265a1b6d9f4
mi2428 <mi2428782020@gmail.com>
2026-07-26 02:27:29 +0900

    fix(nvim): restore ufo folds after re-enabling

--- home/files/config/nvim/lua/plugins/folding.lua
+++ home/files/config/nvim/lua/plugins/folding.lua
@@ -13,7 +13,24 @@ return {
 			end,
 		},
 		config = function(_, opts)
-			require("ufo").setup(opts)
+			local ufo = require("ufo")
+			ufo.setup(opts)
+
+			-- UFO does not rebuild its manual folds when 'foldenable' is
+			-- switched back on with the built-in zi/zN commands.
+			vim.keymap.set("n", "zi", function()
+				if vim.wo.foldenable then
+					vim.wo.foldenable = false
+				else
+					vim.wo.foldenable = true
+					ufo.enableFold()
+				end
+			end, { desc = "Toggle folds" })
+
+			vim.keymap.set("n", "zN", function()
+				vim.wo.foldenable = true
+				ufo.enableFold()
+			end, { desc = "Enable folds" })
 		end,
 	},
 }
