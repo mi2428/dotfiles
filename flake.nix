569a2ec66c0f14b8854d2d7e8ee76fff7407da8a
mi2428 <mi2428782020@gmail.com>
2026-08-10 02:14:18 +0900

    chore: update lingua-motion to 0.9.1

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    lingua-motion.url = "git+ssh://git@github.com/mi2428/lingua-motion.nvim.git?ref=refs/tags/v0.9.0";
+    lingua-motion.url = "git+ssh://git@github.com/mi2428/lingua-motion.nvim.git?ref=refs/tags/v0.9.1";
     lingua-motion.inputs.nixpkgs.follows = "nixpkgs";
   };
 
